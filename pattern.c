324516c71c1f001e893a45d413318a19276b5bf9
Ankitajadhav4803 <142647278+Ankitajadhav4803@users.noreply.github.com>
2023-10-20 17:55:52 +0530

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,21 @@
+/* print the following pattern 
+	*****
+	*****
+	*****
+	*****
+	*****	*/
+#include<stdio.h>
+void main()
+{
+	int row,col,n;
+	printf("enter limit=");
+	scanf("%d",&n);
+	for(row=1;row<=n;row++)
+	{
+	for(col=1;col<=n;col++)
+	{
+	printf("*");
+	}
+	printf("\n");
+	}
+}
